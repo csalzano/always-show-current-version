8d8b350b636e329614aecd4892e8e002575a3873
Corey Salzano <corey.salzano@gmail.com>
2019-07-13 17:30:08 -0400

    First build

---
+++ always-show-current-version.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Always Show Current Version in Dashboard Footer
+ * Plugin URI:
+ * Description: Shows the current WordPress version number in the Dashboard footer even when an update is available.
+ * Version: 1.0.0
+ * Author: Corey Salzano
+ * Author URI: https://profiles.wordpress.org/salzano
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+add_filter( 'update_footer', 'salzano_persist_version_number', 11 );
+
+function salzano_persist_version_number( $value ) {
+	$version = get_bloginfo( 'version', 'display' );
+	if( false !== strpos( $value, $version ) ) {
+		return $value;
+	}
+	return sprintf( __( ' Version %s, ' ), $version ) . $value;
+}
